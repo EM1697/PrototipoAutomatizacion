1c3290f40f73022d2e209361711d5e41da98fa77
Jared <jaredlanderos97@gmail.com>
2019-10-30 10:54:01 -0700

    Programa de temperatura el shido

--- Temp.py
+++ Temp.py
@@ -1,56 +1,34 @@
 import os
-
 import glob
-
 import time
 
+#http://www.innovadomotics.com/mn-tuto/mn-mod/mn-rp/11-raspberry-pi-ds18b20.html
 
 os.system('modprobe w1-gpio')
-
 os.system('modprobe w1-therm')
 
+_direccion = '/sys/bus/w1/devices/'
+dispositivo_folder = glob.glob(_direccion + '28*')[0]
+dispositivo_pad = dispositivo_folder + '/w1_slave'
 
-base_dir = '/sys/bus/w1/devices/'
+def leer_temperatura():
+    f = open(dispositivo_pad, 'r')
+    lineas = f.readlines()
+    f.close()
+    return lineas
 
-device_folder = glob.glob(base_dir + '28*')[0]
-
-device_file = device_folder + '/w1_slave'
-
-
-def read_temp_raw():
-
-f = open(device_file, 'r')
-
-lines = f.readlines()
-
-f.close()
-
-return lines
-
-
-def read_temp():
-
-lines = read_temp_raw()
-
-while lines[0].strip()[-3:] != 'YES':
-
-time.sleep(0.2)
-
-lines = read_temp_raw()
-
-equals_pos = lines[1].find('t=')
-
-if equals_pos != -1:
-
-temp_string = lines[1][equals_pos+2:]
-
-temp_c = float(temp_string) / 1000.0
-
-return temp_c
-
-
+def determinar_valores():
+    lineas = leer_temperatura()
+    while lineas[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lineas = leer_temperatura()
+    igual_pos = lineas[1].find('t=')
+    if igual_pos != -1:
+        temp_string = lineas[1][igual_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
 while True:
-
-print("La temp es: "+str(read_temp())+" ˚C")
-
-time.sleep(0.5)
+ print("centigrados,fahrenheit")
+ print(determinar_valores())
+ time.sleep(1)
